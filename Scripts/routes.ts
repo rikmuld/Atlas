f19c9db5029204a94d6f219621dd95e8ddf6fbd8
RikMuld <rikmuld@gmail.com>
2016-04-08 21:02:37 +0200

    created a basic server

---
+++ Scripts/routes.ts
@@ -0,0 +1,9 @@
+﻿import express = require('express');
+
+export function setup(app: express.Express, io: SocketIO.Server) {
+    app.get('/', displayGame)
+}
+
+function displayGame(req: express.Request, res: express.Response) {
+    res.send('atlas')
+}
---
+++ Scripts/server.ts
@@ -0,0 +1,23 @@
+﻿//Importing neccecities
+import express = require('express');
+import path = require('path')
+import stylus = require('stylus')
+import routes = require('./routes')
+import socket = require('socket.io')
+
+//Creating server
+var app = express()
+var server = require('http').Server(app)
+var io = socket(server)
+
+//Configuration server
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'views'))
+app.use(stylus.middleware(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Setup routings
+routes.setup(app, io)
+
+//Starting server
+server.listen(3000)
