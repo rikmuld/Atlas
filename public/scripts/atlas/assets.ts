e34499b2f6104a07b40e0ac74be3209722da94ae
RikMuld <rikmuld@gmail.com>
2016-05-21 03:49:09 +0200

    removed old refrence to star image and updated version number

--- public/scripts/atlas/assets.ts
+++ public/scripts/atlas/assets.ts
@@ -1,10 +1,8 @@
 ﻿module Textures {
-    export let starImg: Img
     export let worldSprite: Sprite
     export let mapImg: Img
 
     export function load() {
-        starImg = Assets.loadImg("/images/stars.jpg", Assets.BACKGROUND)
         worldSprite = Assets.loadSprite("/images/worldUtils.png", Assets.TILE_SHEET)
         mapImg = Assets.loadImg("/images/worldMap.png", Assets.NORMAL)
 
